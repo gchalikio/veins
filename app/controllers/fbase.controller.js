b0b986fc440105c3b011c01a2473d052057cf372
George Chalikiopoulos <chalikiopoulosgr@gmail.com>
2018-11-14 12:16:22 +0200

    mssql finished, model

--- app/controllers/fbase.controller.js
+++ app/controllers/fbase.controller.js
@@ -1,19 +1,17 @@
-const appServer = require('../functions/respond');
+const api = require('../functions/respond');
+const Product = require("../models/product");
 const fbase = require('../config/connection');
 const firestore = fbase.fbaseConfig();
 
 exports.create = function (req, res, next) {
-    let product = {
-        name: req.body.name,
-        price: req.body.price
-    };
+    const product = new Product(req.params.name, req.params.price);
 
     firestore.collection('products').add(product)
     .then(ref => {
-        appServer.answers(res, 200, true, "Created product with ID", ref.id);
+        api.answers(res, 200, true, "Created product with ID", ref.id);
     })
     .catch((err) => {
-        appServer.answers(res, 500, false, err, null);
+        api.answers(res, 500, false, err, null);
     });
 };
 
@@ -32,17 +30,17 @@ exports.read = async function (req, res) {
         });
     })
     .catch((err) => {
-        appServer.answers(res, 500, false, err, null);
+        api.answers(res, 500, false, err, null);
     });
     
-    appServer.answers(res, 200, true, "fbase", data);
+    api.answers(res, 200, true, "fbase", data);
 };
 
 exports.readOne = function (req, res) {
     firestore.collection('products').doc(req.params.id).get()
     .then(doc => {
         if (!doc.exists) {
-            appServer.answers(res, 200, false, "No such document!", null);
+            api.answers(res, 200, false, "No such document!", null);
         } else {
             let temp = doc.data();
             let product = { 
@@ -50,30 +48,27 @@ exports.readOne = function (req, res) {
                 name: temp.name,
                 price: temp.price
             } 
-            appServer.answers(res, 200, true, 'Document data', product);
+            api.answers(res, 200, true, 'Document data', product);
         }
     })
     .catch(err => {
         console.log('Error getting document', err);
-        appServer.answers(res, 200, false, "fbase", data);
+        api.answers(res, 200, false, "fbase", data);
     });  
 };
 
 exports.update = function (req, res) {
-    // let product = {
-    //     name: req.body.name,
-    //     price: req.body.price
-    // };
+    const product = new Product(req.params.name, req.params.price);
     firestore.collection('products').doc(req.params.id).update(
         {
             price: 100    
         }
     )
     .then(ref => {
-        appServer.answers(res, 200, true, "Updated product with ID", req.params.id);
+        api.answers(res, 200, true, "Updated product with ID", req.params.id);
     })
     .catch((err) => {
-        appServer.answers(res, 500, false, err, null);
+        api.answers(res, 500, false, err, null);
     });
 
 };
@@ -81,9 +76,9 @@ exports.update = function (req, res) {
 exports.delete = function (req, res) {
     firestore.collection('products').doc(req.params.id).delete()
     .then(ref => {
-        appServer.answers(res, 200, true, "Deleted product with ID", req.params.id);
+        api.answers(res, 200, true, "Deleted product with ID", req.params.id);
     })
     .catch((err) => {
-        appServer.answers(res, 500, false, err, null);
+        api.answers(res, 500, false, err, null);
     });
 };
